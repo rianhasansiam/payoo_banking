8035d0c08cd4acbb46b3c634c5dd6eaaf5086a5d
rianhasansiam <rianhasan1971@gmail.com>
2024-09-20 04:01:48 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// console.log()
+
+
+
+document.getElementById('login-btn').addEventListener('click',function(){
+
+    const phoneInput=document.getElementById('phone-input').value;
+
+    const pinInputt=document.getElementById('pin-input').value;
+
+    if(phoneInput=="01932600504"  && pinInputt=="0193"){
+
+        // window.location.href="./home.html";
+
+    }
+    else{
+        alert("Your phone or pin is wrong.please try again later...!");
+    }
+
+})
